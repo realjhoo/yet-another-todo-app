328b18c2f5a29d4faf04f8d60cb78a2d253b1e17
Jerry L Hoover <jerry.hoover@ymail.com>
2020-11-02 19:46:40 -0600

    reinstall express

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,7 +1,4 @@
 export class Todo {
-  // splice(editIndex: string, arg1: number, TodoList: Todo) {
-  //   throw new Error('Method not implemented.');
-  // }
   name: string;
   editState: boolean;
 }
